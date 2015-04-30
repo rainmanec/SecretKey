17e389e39ba0023f112b8b6fd494bffe2ce339c0
rainmanec <rainmancumt@163.com>
2015-04-30 09:59:10 +0800

    edit

---
+++ SecretKey/SecretKey/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace SecretKey
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tb_mac = new System.Windows.Forms.TextBox();
+            this.tb_hash = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tb_xlh = new System.Windows.Forms.TextBox();
+            this.btn_submit = new System.Windows.Forms.Button();
+            this.btn_mac = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tb_mac
+            // 
+            this.tb_mac.Location = new System.Drawing.Point(109, 58);
+            this.tb_mac.Multiline = true;
+            this.tb_mac.Name = "tb_mac";
+            this.tb_mac.Size = new System.Drawing.Size(315, 118);
+            this.tb_mac.TabIndex = 1;
+            // 
+            // tb_hash
+            // 
+            this.tb_hash.Location = new System.Drawing.Point(109, 20);
+            this.tb_hash.Name = "tb_hash";
+            this.tb_hash.Size = new System.Drawing.Size(315, 21);
+            this.tb_hash.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(47, 62);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 12);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "MAC地址：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(65, 23);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "HASH：";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(53, 195);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 12);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "序列号：";
+            // 
+            // tb_xlh
+            // 
+            this.tb_xlh.Location = new System.Drawing.Point(109, 192);
+            this.tb_xlh.Multiline = true;
+            this.tb_xlh.Name = "tb_xlh";
+            this.tb_xlh.Size = new System.Drawing.Size(315, 117);
+            this.tb_xlh.TabIndex = 3;
+            // 
+            // btn_submit
+            // 
+            this.btn_submit.Location = new System.Drawing.Point(118, 327);
+            this.btn_submit.Name = "btn_submit";
+            this.btn_submit.Size = new System.Drawing.Size(107, 23);
+            this.btn_submit.TabIndex = 4;
+            this.btn_submit.Text = "生成";
+            this.btn_submit.UseVisualStyleBackColor = true;
+            this.btn_submit.Click += new System.EventHandler(this.btn_submit_Click);
+            // 
+            // btn_mac
+            // 
+            this.btn_mac.Location = new System.Drawing.Point(290, 327);
+            this.btn_mac.Name = "btn_mac";
+            this.btn_mac.Size = new System.Drawing.Size(107, 23);
+            this.btn_mac.TabIndex = 8;
+            this.btn_mac.Text = "重新获取MAC地址";
+            this.btn_mac.UseVisualStyleBackColor = true;
+            this.btn_mac.Click += new System.EventHandler(this.btn_mac_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(700, 434);
+            this.Controls.Add(this.btn_mac);
+            this.Controls.Add(this.btn_submit);
+            this.Controls.Add(this.tb_xlh);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tb_hash);
+            this.Controls.Add(this.tb_mac);
+            this.Name = "Form1";
+            this.Text = "密钥生成器";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tb_mac;
+        private System.Windows.Forms.TextBox tb_hash;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tb_xlh;
+        private System.Windows.Forms.Button btn_submit;
+        private System.Windows.Forms.Button btn_mac;
+    }
+}
+
---
+++ SecretKey/SecretKey/Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Management;
+
+namespace SecretKey
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.InitMacs();
+        }
+
+        #region  辅助函数
+        public string EncodeHash(string str)
+        {
+            string SECRET_KEY = this.tb_hash.Text.Trim();
+            str = SECRET_KEY + str + SECRET_KEY;
+            return this.GetMd5(this.GetMd5(str));
+        }
+        /// <summary>
+        /// Array拼接成字符串
+        /// </summary>
+        /// <param name="sp"></param>
+        /// <param name="arr"></param>
+        /// <returns></returns>
+        public string ArrayJoin(string sp, string[] arr)
+        {
+            string result = "";
+            for (int i = 0, length = arr.Length; i < length; i++)
+            {
+                result += arr[i];
+                if (i != length - 1)
+                {
+                    result += sp;
+                }
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// List转换为Array
+        /// </summary>
+        /// <param name="list"></param>
+        /// <returns></returns>
+        public string[] ListToArray(List<string> list)
+        {
+            string[] result = new string[list.Count];
+            for (int i = 0, length = list.Count; i < length; i++)
+            {
+                result[i] = list[i];
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// Md5加密字符串
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public string GetMd5(string str)
+        {
+            System.Security.Cryptography.MD5CryptoServiceProvider md5Hasher = new System.Security.Cryptography.MD5CryptoServiceProvider();
+            byte[] hashedDataBytes;
+            hashedDataBytes = md5Hasher.ComputeHash(Encoding.GetEncoding("gb2312").GetBytes(str));
+            StringBuilder tmp = new StringBuilder();
+            foreach (byte i in hashedDataBytes)
+            {
+                tmp.Append(i.ToString("x2"));
+            }
+            str = tmp.ToString().ToUpper();
+            return str;
+        }
+
+        /// <summary>
+        /// 获取网卡地址
+        /// </summary>
+        /// <returns></returns>
+        public string[] GetMacAddress()
+        {
+            ManagementClass mc = new ManagementClass("Win32_NetworkAdapterConfiguration");
+            ManagementObjectCollection moc = mc.GetInstances();
+            List<string> list = new List<string>();
+            foreach (ManagementObject mo in moc)
+            {
+                if (mo["MacAddress"] != null)
+                {
+                    list.Add(mo["MacAddress"].ToString().Replace('-', ':').ToUpper());
+                }
+            }
+            return this.ListToArray(list);
+        }
+
+        public void InitMacs()
+        {
+            string[] macs = this.GetMacAddress();
+            tb_mac.Text = this.ArrayJoin("\r\n", macs);
+        }
+
+        #endregion
+
+        private void btn_submit_Click(object sender, EventArgs e)
+        {
+            string[] macs = this.tb_mac.Text.Trim().Split(new string[] { "\r\n" }, StringSplitOptions.None);
+            List<string> list = new List<string>();
+            foreach (string m in macs)
+            {
+                if (m.Trim() != "")
+                {
+                    list.Add(this.EncodeHash(m.Trim()));
+                }
+                else
+                {
+                    list.Add(m);
+                }
+            }
+            this.tb_xlh.Text = this.ArrayJoin("\r\n", this.ListToArray(list));
+        }
+
+        private void btn_mac_Click(object sender, EventArgs e)
+        {
+            this.InitMacs();
+        }
+
+
+    }
+}
